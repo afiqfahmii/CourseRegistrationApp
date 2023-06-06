833c515cfb8d748be55952b4dc505889f532c242
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-06-06 14:06:45 +0800

    Update

--- src/App.java
+++ src/App.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        //Print menu
+       printMenu();
 
-
-        
-
-
+        //
         // Student student1 = new Student();
         Scanner s = new Scanner(System.in);
-        Course c1 = new Course("OOP","SECJ2020",3);
+        Course c1 = new Course("OOP", "SECJ2020", 3);
 
         System.out.println("Enter student details:");
         System.out.print("Name: ");
@@ -17,19 +17,30 @@ public class App {
         System.out.print("Age: ");
         int age = s.nextInt();
         s.nextLine();
-        
+
         System.out.print("Email: ");
         String email = s.nextLine();
-        
+
         System.out.print("Address: ");
         String address = s.nextLine();
 
         System.out.print("MatricNumber: ");
         String matricNumber = s.nextLine();
 
-
-        Student student = new Student(name, age, address, email ,matricNumber);
+        Student student = new Student(name, age, address, email, matricNumber);
         c1.addStudent(student);
 
+        s.close();
+    }
+
+    public static void printMenu(){
+    //Print menu details here
+    System.out.println("+--------------------------------------------------------------+");
+    System.out.println("+------------------- FACULTY OF COMPUTING ---------------------+");
+    System.out.println("+-------------------- COURSE REGISTRATION ---------------------+");
+    System.out.println("+--------------------------------------------------------------+");
+    System.out.println("1. Log In");
+    System.out.println("2. Forgot Password");
+    System.out.println("3. Exit");
     }
 }
